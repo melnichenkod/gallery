b7bdba1531f7999dfa62347ca4fb3910a984e9c2
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2021-11-19 13:23:53 +0200

    added manageHTML method, wrapElementByDiv function helper

--- gallery-lib/gallery.js
+++ gallery-lib/gallery.js
@@ -4,12 +4,36 @@ const gallerySlideClassName = 'gallery-slide'
 
 class Gallery{
   constructor(element, options = {}) {
-    this.conainerNode = element;
+    this.containerNode = element;
     this.size = element.childElementCount;
     this.currentSlide = 0;
+    this.manageHTML = this.manageHTML.bind(this)
     this.manageHTML();
   }
   manageHTML() {
-    this.conainerNode.classList.add(galleryClassName)
+    this.containerNode.classList.add(galleryClassName);
+    this.containerNode.innerHTML = `
+      <div class="${galleryLineClassName}">
+        ${this.containerNode.innerHTML}
+      </div>
+    `;
+    this.lineNode = this.containerNode.querySelector(`.${galleryLineClassName}`);
+    this.slideNodes = Array.from(this.lineNode.children).map(childNode => 
+      wrapElementByDiv({
+        element: childNode,
+        className: gallerySlideClassName
+      })
+    )
+      console.log(this.slideNodes);
   }
+}
+
+//Helpers
+function wrapElementByDiv({element, className}) {
+  const wrapperNode = document.createElement('div');
+  wrapperNode.classList.add(className);
+  element.parentNode.insertBefore(wrapperNode, element);
+  console.log(element.parentNode);
+  wrapperNode.appendChild(element);
+  return wrapperNode;
 }
