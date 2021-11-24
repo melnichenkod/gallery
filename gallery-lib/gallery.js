87c28b211aeb8852a7ab41364f23422f2870f8d4
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2021-11-24 10:28:15 +0200

    added gallery navigation buttons, gallery dots

--- gallery-lib/gallery.js
+++ gallery-lib/gallery.js
@@ -2,6 +2,12 @@ const GalleryClassName = 'gallery';
 const GalleryDraggableClassName = 'gallery-draggable';
 const GalleryLineClassName = 'gallery-line';
 const GallerySlideClassName = 'gallery-slide';
+const GalleryDotsClassName = 'gallery-dots';
+const GalleryDotClassName = 'gallery-dot';
+const GalleryDotActiveClassName = 'gallery-dot-active';
+const GalleryNavClassName = 'gallery-nav';
+const GalleryNavLeftClassName = 'gallery-nav-left';
+const GalleryNavRightClassName = 'gallery-nav-right';
 
 class Gallery{
   constructor(element, options = {}){
@@ -33,8 +39,14 @@ class Gallery{
       <div class="${GalleryLineClassName}">
         ${this.containerNode.innerHTML}
       </div>
+      <div class="${GalleryNavClassName}">
+        <button class="${GalleryNavLeftClassName}">Left</button>
+        <button class="${GalleryNavRightClassName}">Right</button>
+      </div>
+      <div class="${GalleryDotsClassName}"></div>
     `;
     this.lineNode = this.containerNode.querySelector(`.${GalleryLineClassName}`);
+    this.dotsNode = this.containerNode.querySelector(`.${GalleryDotsClassName}`);
 
     this.slideNodes = Array.from(this.lineNode.children).map((childNode) =>
       wrapElementByDiv({
@@ -42,6 +54,10 @@ class Gallery{
         className: GallerySlideClassName
       })
     );
+
+    this.dotsNode.innerHTML = Array.from(Array(this.size).keys()).map((key) => (
+      `<button class="${GalleryDotClassName} ${key === this.currentSlide ? GalleryDotActiveClassName : ''}"</button>`
+    )).join('');
   }
 
   setParameters(){
